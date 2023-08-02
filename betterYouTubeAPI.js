82882621d006931914b637154a2cfd70497aae7a
Dorge47 <Dorge47@users.noreply.github.com>
2023-08-01 20:38:46 -0700

    Add getVid() and start Stream()

--- betterYouTubeAPI.js
+++ betterYouTubeAPI.js
@@ -1,5 +1,6 @@
 const https = require("https");
-const ytAPIKey = JSON.parse(fs.readFileSync("/apikey"));
+const fs = require('fs');
+const ytAPIKey = JSON.parse(fs.readFileSync("apikey"));
 function sendRequest(func, params) {
     let urlQuery = new URLSearchParams(params).toString();
     const options = {
@@ -20,13 +21,60 @@ function sendRequest(func, params) {
         });
     });
 };
+function Stream(id, title, tags, startTime, status) {
+    this.id = id;
+    this.title = title;
+    this.tags = tags;
+    this.startTime = startTime;
+    this.status = status;
+    this.getTimeRemaining = function() {
+        
+    };
+};
 exports.getUploadListById = function(channelId) {
     var uploadPlaylistId = "UU" + channelId.slice(2);
     return uploadPlaylistId;
 };
 exports.getFutureVids = async function(channelId, apiKey) {
     
-}
+};
+exports.getVid = async function(videoId) {
+    let status = "";
+    let title = "";
+    let tags = [];
+    let startTime = "";
+    let requestParams = {
+        "part": "contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails",
+        "id": videoId,
+        "key": ytAPIKey
+    }
+    let response = await sendRequest("videos", requestParams);
+    let responseObj = JSON.parse(response);
+    if (responseObj.pageInfo.totalResults == 0) {
+        status = "missing";
+    }
+    else if (responseObj.items[0].liveStreamingDetails === undefined) {
+        // Video is not a stream
+        return null;
+    };
+    if (responseObj.items[0].snippet.liveBroadcastContent == "live") {
+        status = "live";
+    }
+    else if (responseObj.items[0].snippet.liveBroadcastContent == "upcoming") {
+        status = "upcoming";
+    }
+    else if (responseObj.items[0].liveStreamingDetails.actualEndTime !== undefined) {
+        status = "past";
+    };
+    title = responseObj.items[0].snippet.title;
+    tags = responseObj.items[0].snippet.tags;
+    startTime = responseObj.items[0].liveStreamingDetails.actualStartTime || responseObj.items[0].liveStreamingDetails.scheduledStartTime;
+    return new Stream(videoId, title, tags, startTime, status);
+};
+/*async function testYT() {
+    output = await exports.getVid("");
+    console.log(output);
+}*/
 /*var requestParams = {
     "part": "status,snippet,id,contentDetails",
     "playlistId": "",
